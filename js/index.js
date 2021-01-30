9228020e6ebdd19f984a5c66734af6f5bdb5146e
Jeev <findjeev@gmail.com>
2021-01-30 10:21:46 -0600

    removed inline css & js, added js documentation

--- js/index.js
+++ js/index.js
@@ -1,61 +1,75 @@
 var Resources = {
+    // state variables
     resourceData: [],
     activeType: '',
     activeSort: '',
 
+    // DOM element variables (denoted by '$')
     $cardsContainer: null,
     $noResults: null,
     $resourceCount: null,
     $searchInput: null,
     $typeInput: null,
     $sortInput: null,
+    $clearBtn: null,
 
     loadJSON() {
         var xmlhttp = new XMLHttpRequest()
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
+                // if successful, store JSON in resourceData array
                 Resources.resourceData = JSON.parse(this.responseText)
+                // set up DOM
                 Resources.displayPage()
             }
         }
         xmlhttp.open("GET", "../js/data.json", true)
         xmlhttp.send()
     },
-
     displayPage() {
+        // initialize DOM element variables
         this.$cardsContainer = document.querySelector('#cards-container')
         this.$noResults = document.querySelector('#no-results')
         this.$resourceCount = document.querySelector('#resource-count')
         this.$searchInput = document.querySelector('#search-input')
         this.$typeInput = document.querySelector('#type-input')
         this.$sortInput = document.querySelector('#sort-input')
+        this.$clearBtn = document.querySelector('#clear-btn')
 
+        // initialize state variables as default DOM variable values
         this.activeType = this.$typeInput.value
         this.activeSort = this.$sortInput.value
 
+        // render resource cards with JSON data 
         this.renderCards()
+
+        // establish listeners for interactive DOM elements
         this.setListeners()
         
+        // set up resource type & sort type variables
         this.updateType()
         this.updateSort()
     },
-
     renderCards() {
-
         this.resourceData.forEach(el => {
-            console.log(el.name)
+            // add divs for card and card content to the DOM
             let $cardItem = document.createElement("div")
             let $cardBody = document.createElement("div")
             
+            // set data-name attribute (resource name) & Bootstrap classes
             $cardItem.className = 'card my-2';
             $cardItem.setAttribute('data-name', el.name)
             $cardBody.className = 'card-body';
     
+            // create string to build card's HTML content
             let cardContent = `
                 <h5 class="card-title"><a href="${el.link}" target="_blank">${el.name} (${el.type})</a></h5>
             `
+
+            // create string for payment rate information
             let rateInfo = '<p class="card-text">'
-    
+
+            // determine relevant payment rate information based on resource type
             switch (el.type) {
                 case 'publication':
                     rateInfo += el.minRate ? `<strong>$${el.minRate} &ndash; </strong>` : ''
@@ -75,11 +89,11 @@ var Resources = {
                     break;
             }
             
+            // add payment rate information to resource card
             rateInfo += '</p>'
-            cardContent += rateInfo
-    
-            cardContent += '<div class="w-100">'
+            cardContent += rateInfo + '<div class="w-100">'
             
+            // create labels for each resource topic
             let cardTopics = ''
             if (el.topics) {
                 el.topics.forEach(topic => {
@@ -87,50 +101,77 @@ var Resources = {
                 }); 
             }
     
+            // add group of resource topics to the card
             cardContent += '</div>'
             cardContent += `<div class="w-100">${cardTopics}</div>`
     
+            // add all card content HTML to the card
             $cardBody.innerHTML = cardContent
     
+            // append card body to parent card div
             $cardItem.appendChild($cardBody)
+
+            // append card to container div
             this.$cardsContainer.appendChild($cardItem)
 
+            // create resource count message with total number of resources
             this.$resourceCount.textContent = `Displaying ${this.resourceData.length} resources`
-
         });
-        
     },
-
     setListeners() {
+        // handle search each time a key is pressed
         this.$searchInput.addEventListener("keyup", () => {
             this.handleSearch(this.$searchInput.value)
         });
-        this.$sortInput.addEventListener("onchange", () => {
-            this.handleSearch()
+
+        // re-sort the array each time the sort select is changed
+        this.$sortInput.addEventListener("change", () => {
+            this.updateSort()
+            this.handleSearch(this.$searchInput.value)
+        });
+
+        // update resource type when type select is changed
+        this.$typeInput.addEventListener("change", () => {
+            console.log("type is changed")
+            this.updateType()
+        });
+
+        // clear search field when clear button is clicked
+        this.$clearBtn.addEventListener("click", () => {
+            this.handleSearch('')
         });
     },
     updateType() {
+        // set active type to value in type select
         this.activeType = this.$typeInput.value
+
+        // run search function to display updated results
         this.handleSearch(this.$searchInput.value)
     },
     updateSort() {
+        // set active sort to value in sort select
         this.activeSort = this.$sortInput.value
 
+        // get all elements with 'card' class in cards container and remove them from DOM
         var $allItems = this.$cardsContainer.getElementsByClassName('card')
         while($allItems.length > 0){
             $allItems[0].parentNode.removeChild($allItems[0]);
         }
 
+        // rebuild DOM with 
         switch (this.activeSort) {
             case 'alpha':
+                // sort array alphabetically by resource name
                 this.resourceData.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
                 break;
-            case 'rate_increasing':
+                case 'rate_increasing':
+                // sort array by resource payment rate in increasing order
                 this.resourceData.sort((a, b) => (
                     (a.maxRate || a.hourlyMaxRate) > (b.maxRate || b.hourlyMaxRate) ? 1 : -1
                 ))
                 break;
             case 'rate_decreasing':
+                // sort array by resource payment rate in decreasing order
                 this.resourceData.sort((a, b) => (
                     (a.maxRate || a.hourlyMaxRate) < (b.maxRate || b.hourlyMaxRate) ? 1 : -1
                 ))
@@ -139,17 +180,27 @@ var Resources = {
                 break;
         }
 
+        // re-render cards in the DOM with sorted resource array
         this.renderCards()
+
+        // run update type function to display updated results
         this.updateType()
 
     },
     handleSearch(val) {
+        // set search field input value to search term
         this.$searchInput.value = val
         var search = val.toLowerCase();
+
+        // loop through resources in resource data array
         this.resourceData.forEach(el => {
+            // reference element by class and data-name attribute value
             const $el = document.querySelector(`.card[data-name="${el.name}"]`)
+
+            // remove class that hides element
             $el.classList.remove('d-none')
     
+            // create array for any topics included in the search term
             let searchedTopics = []
             if (el.topics) {
                 searchedTopics = el.topics.filter(topic => {
@@ -157,6 +208,9 @@ var Resources = {
                 })
             }
     
+            // make element visible if:
+            //    1) search term includes resource name or >= 1 resource topic
+            //    2) resource type is 'everything' or the active type
             if (
                 (el.name.toLowerCase().includes(search) || searchedTopics.length) &&
                 (el.type == this.activeType || this.activeType == 'everything')
@@ -168,12 +222,19 @@ var Resources = {
             }
         });
 
+        // get number of elements with hidden class in cards container
         let numberHidden = this.$cardsContainer.getElementsByClassName('d-none').length
+
+        // get total number of resources
         let totalItems = this.resourceData.length
+
+        // calculate total number of cards shown (total cards minus hidden cards)
         let numberShown = totalItems - numberHidden
 
+        // create string for number of resources shown
         let resourceCountText = `Displaying ${numberShown} `
 
+        // add type of resource to resource count string
         switch (this.activeType) {
             case 'agency':
                 resourceCountText += numberShown != 1 ? 'agencies' : 'agency'
@@ -189,15 +250,18 @@ var Resources = {
                 break;
         }
 
+        // set text content of DOM element to resource count string
         this.$resourceCount.textContent = resourceCountText
 
+        // only display "no results" message if no resources are shown
         this.$noResults.classList.add('d-none')
-        if (totalItems - numberHidden == 0) {
+        if (numberShown == 0) {
             this.$noResults.classList.remove('d-none')
         }
     },
 }
 
+// on window load, run function to load/parse JSON file
 window.onload = () => {
     Resources.loadJSON()
 }
