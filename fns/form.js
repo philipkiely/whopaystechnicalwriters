a09939ed50d707e4bbf15c9d43d0da749f798153
Philip Kiely <kielyphi@grinnell.edu>
2021-02-03 12:30:26 -0600

    correct https lib

--- fns/form.js
+++ fns/form.js
@@ -1,36 +1,49 @@
+const https = require('https')
+
 exports.handler = async (event, context) => {
 
     if (event.httpMethod !== "POST") {
         return { statusCode: 405, body: "Method Not Allowed" };
     }
 
-    var emailData = {
+    var emailData = JSON.stringify({
         "personalizations": [{ "to": [{ "email": "philip@kiely.xyz" }] }],
         "from": { "email": "philip@kiely.xyz" },
         "subject": "Who Pays Technical Writers Form Submission",
         "content": [{ "type": "text/plain", "value": event.body }]
-                    }
+                    });
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://api.sendgrid.com/v3/mail/send", true);
 
-    xhr.setRequestHeader("Authorization", "Bearer " + process.env.SENDGRID_API_KEY);
+    const options = {
+        hostname: 'api.sendgrid.com',
+        port: 443,
+        path: '/v3/mail/send',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + process.env.SENDGRID_API_KEY
+        }
+    }
 
-    xhr.setRequestHeader("Content-Type", "application/json");
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        res.on('data', d => {
             return {
                 statusCode: 200,
                 body: "Success"
             };
-        } else {
-            return {
-                statusCode: 500,
-                body: "Sendgrid failed."
-            };
-        }
-    }
+        })
+    })
 
-    xhr.send(JSON.stringify(emailData));
-};
+    req.on('error', error => {
+        return {
+            statusCode: 500,
+            body: "Sendgrid failed."
+        };
+    })
+
+    req.write(emailData)
+    req.end()
+
+}
