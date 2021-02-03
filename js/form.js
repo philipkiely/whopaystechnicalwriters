799fc65c7e83c1e1780b074558af71d600ecaa1f
Philip Kiely <kielyphi@grinnell.edu>
2021-02-02 19:49:14 -0600

    checkpoint

--- js/form.js
+++ js/form.js
@@ -1,7 +1,45 @@
-// Grab the data
-// Validate that 3 fields are there
-// JSON it
-// Send it
-// Clear Form
-// Message the user success
+function errorRequiredField(id, text) {
+    console.log("errorReq", id, text) // show message, highlight field, etc
+    document.getElementById("form-failed").hidden = false
+}
 
+function processForm(event) {
+    event.preventDefault();
+    // Gather Data
+    var companyName = document.getElementById("company-name").value;
+    var companyType = document.getElementById("company-type").value;
+    var link = document.getElementById("link").value;
+    var email = document.getElementById("email").value;
+    var topics = document.getElementById("topics").value;
+    var rates = document.getElementById("rates").value;
+    var notes = document.getElementById("notes").value;
+    // Validate Required Fields (companyType is never empty)
+    if (companyName == "") {
+        errorRequiredField("company-name", "Please enter a company name.")
+        return
+    }
+    if (link == "") {
+        errorRequiredField("link", "Please enter a link to the company's \"write for us\" page.")
+        return
+    }
+    // JSON Data
+    var formData = {
+        "company-name": companyName,
+        "companyType": companyType,
+        "link": link,
+        "email": email,
+        "topics": topics,
+        "rates": rates,
+        "notes": notes
+    }
+    // Send Data
+    console.log(formData)
+    // Clear Form
+    document.getElementById("new-pub-form").reset();
+    // Alert Success
+    document.getElementById("form-failed").hidden = true
+    document.getElementById("form-succeeded").hidden = false
+}
+
+var form = document.getElementById("new-pub-form");
+form.addEventListener("submit", processForm);
